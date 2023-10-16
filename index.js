5707c26c79582985991158c58f59d5a32c7ce83d
Gaurav Kumar <97530398+Gaurav01Kumar@users.noreply.github.com>
2023-10-16 15:38:44 +0530

    first

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const config = require("./config");
+const config = require("./src/config");
 const http = require("http");
-const app = require("./app");
+const app = require("./src/app");
 const server = http.createServer(app);
 server.listen(config.PORT, () => {
   console.log("Server is running on port 8090");
