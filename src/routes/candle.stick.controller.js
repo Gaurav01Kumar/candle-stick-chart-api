0608f0762da02f2c46d638993a0dfd971696d80e
Gaurav Kumar <97530398+Gaurav01Kumar@users.noreply.github.com>
2023-10-16 20:39:05 +0530

    Update candle.stick.controller.js

--- src/routes/candle.stick.controller.js
+++ src/routes/candle.stick.controller.js
@@ -1,49 +1,30 @@
-const config = require("../config");
-
-
-function generateRandomCandlesticks(numCandles) {
-  const candlesticks = [];
-
-  for (let i = 0; i < numCandles; i++) {
-    // Generate random open, high, low, and close prices
-    const open = getRandomPrice(100, 200); // Replace with appropriate range
-    const high = open + getRandomPrice(5, 20); // Example: High is usually slightly higher than open
-    const low = open - getRandomPrice(5, 20);  // Example: Low is usually slightly lower than open
-    const close = getRandomPrice(low, high); // Close price within the high and low
-
-    // Create a timestamp for the candlestick (e.g., in milliseconds)
-    const timestamp = new Date().getTime() - numCandles * 60000 + i * 60000;
-
-    candlesticks.push({
-      timestamp,
-      open,
-      high,
-      low,
-      close,
-    });
-  }
-
-  return candlesticks;
-}
-
-function getRandomPrice(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-const numCandles = 50; // Number of candlesticks you want to generate
-
-
-
+const config=require("../config")
+const axios = require("axios");
 
 const candlestickController = {};
 
 candlestickController.getCandleStickData = async (req, res) => {
-  
   try {
-   let limit=req.query.limit;
-   if(limit===undefined || limit ==='')  limit=50;
-    const data=generateRandomCandlesticks(limit)
-    return res.status(200).json({data})
+  
+    const [dataResponse, schemaResponse] = await Promise.all([
+      axios.get(
+        `${config.API_URL}`
+      ),
+      axios.get(
+        "https://s3.eu-central-1.amazonaws.com/fusion.store/ft/schema/candlestick-chart-schema.json"
+      ),
+    ]);
+
+    const data = dataResponse.data;
+    const convertedData = data.map((item) => ({
+      Date: item[0],
+      Open: item[1],
+      High: item[2],
+      Low: item[3],
+      Close: item[4],
+      Volume: item[5],
+    }));
+    return res.status(200).json({data:convertedData})
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "INTERNAL SERVER ERROR" });
