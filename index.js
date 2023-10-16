aba8a83e232d743f5633c4880a45d7da99daa386
Gaurav Kumar <97530398+Gaurav01Kumar@users.noreply.github.com>
2023-10-16 15:59:13 +0530

    second

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const config = require("./src/config");
 const http = require("http");
 const app = require("./src/app");
+console.log(config.PORT)
 const server = http.createServer(app);
 server.listen(config.PORT, () => {
   console.log("Server is running on port 8090");
--- src/routes/candleStickRoute.js
+++ src/routes/candleStickRoute.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
 const { getCandleStickData } = require("./candle.stick.controller");
 router.get("/candlestick", getCandleStickData);
+
 module.exports = router;
