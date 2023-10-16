74c7aa1219413ab6c1d749cb68842333be7f39dc
Gaurav Kumar <97530398+Gaurav01Kumar@users.noreply.github.com>
2023-10-16 17:40:32 +0530

    Update candle.stick.controller.js

--- src/routes/candle.stick.controller.js
+++ src/routes/candle.stick.controller.js
@@ -1,5 +1,38 @@
 const config = require("../config");
-const axios = require("axios");
+
+
+function generateRandomCandlesticks(numCandles) {
+  const candlesticks = [];
+
+  for (let i = 0; i < numCandles; i++) {
+    // Generate random open, high, low, and close prices
+    const open = getRandomPrice(100, 200); // Replace with appropriate range
+    const high = open + getRandomPrice(5, 20); // Example: High is usually slightly higher than open
+    const low = open - getRandomPrice(5, 20);  // Example: Low is usually slightly lower than open
+    const close = getRandomPrice(low, high); // Close price within the high and low
+
+    // Create a timestamp for the candlestick (e.g., in milliseconds)
+    const timestamp = new Date().getTime() - numCandles * 60000 + i * 60000;
+
+    candlesticks.push({
+      timestamp,
+      open,
+      high,
+      low,
+      close,
+    });
+  }
+
+  return candlesticks;
+}
+
+function getRandomPrice(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+const numCandles = 50; // Number of candlesticks you want to generate
+
+
 
 
 const candlestickController = {};
@@ -7,30 +40,15 @@ const candlestickController = {};
 candlestickController.getCandleStickData = async (req, res) => {
   
   try {
-    const { symbol, interval, startTime, endTime, limit } = req.query;
-
-    const query = `${config.API_URL}?symbol=${symbol || "BTCUSDT"}&interval=${
-      interval || "1m"
-    }&startTime=${startTime || Date.now()}&endTime=${
-      endTime || Date.now() + 86400000
-    }&limit=${limit || 100}`;
-
- await axios.get(query, {
-  headers: {
-    'Content-Type': 'application/json'
-  }
-}).then((response) => {
-  if (response.data) {
-    return res.status(200).json({ data: response.data });
-  } else {
-    return res.status(400).json({ message: "Bad request" });
-  }
-});
-
+   let limit=req.query.limit;
+   if(limit===undefined || limit ==='')  limit=50;
+    const data=generateRandomCandlesticks(limit)
+    return res.status(200).json({data})
   } catch (error) {
-    console.log(error)
-    return res.status(500).json({ error:error });
+    console.log(error);
+    return res.status(500).json({ error: "INTERNAL SERVER ERROR" });
   }
 };
+
 const { getCandleStickData } = candlestickController;
 module.exports = { getCandleStickData };
